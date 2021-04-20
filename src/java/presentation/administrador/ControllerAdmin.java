d3613012fab8cba7616d15d68d560900a8197172
joeli <joeli@DESKTOP-659CJ3B>
2021-04-20 17:25:02 -0600

    LOGIN LOGOUT

--- src/java/presentation/administrador/ControllerAdmin.java
+++ src/java/presentation/administrador/ControllerAdmin.java
@@ -15,13 +15,28 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import logic.Administrador;
+import logic.Usuario;
 
 @WebServlet(name = "ControllerAdmin", urlPatterns = {"/presentation/administrador/show"})
 public class ControllerAdmin extends HttpServlet {
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        HttpSession sesion = request.getSession();
+        Administrador admin = (Administrador)sesion.getAttribute("usuario");
+        request.setAttribute("model", new ModelAdmin(admin));
+        String viewURL; 
+        switch(request.getServletPath()){
+            case "/presentation/administrador/show": { viewURL = this.showAction(request); break;}
+            default: { viewURL = ""; break; }
+        }
+        request.getRequestDispatcher(viewURL).forward(request, response);
+    }
+    
+    private String showAction(HttpServletRequest request){
+        return "/presentation/administrador/grupos.jsp";
     }
 
     @Override
@@ -35,10 +50,4 @@ public class ControllerAdmin extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
